3a32097a757dc86d579c48ff1e0c73a0e182b80f
sergey878 <sergey.ya93@mail.ru>
2020-03-30 15:52:32 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -1,9 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.corba.se.impl.encoding.CDROutputObject;
+
 public class myFirstProgram {
 
-	public static void main(String args[])	{
-    	System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    hello("would");
+    hello("user");
+    hello("Sergey");
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 
 }
